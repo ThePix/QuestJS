0fd63c19bf648121197b1a2c2f743fa0b66be5c6
ThePix <the_pix@hotmail.com>
2019-08-09 09:33:02 +0100

    Tweaks

--- game4/data.js
+++ game4/data.js
@@ -71,7 +71,7 @@ createRoom("stasis_bay", {
     switch (arr.length) {
       case 0: return "All pods are currently open.";
       case 4: return "Currently only your pod and the spare pod are open.";
-      case 1: return arr[0].byname() + "'s stasis pod is closed.";
+      case 1: return arr[0].byname({possessive:true}) + "'s stasis pod is closed.";
       default: return "The stasis pods of " + formatList(arr) + " are closed.";
     }
   },
