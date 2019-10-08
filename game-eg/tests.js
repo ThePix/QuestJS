82e02571dfdd4392f3a26e311ea763390f05dc3e
ThePix <the_pix@hotmail.com>
2019-10-08 16:02:20 +0100

    Text process can handle attributes like Quest 5 now

--- game-eg/tests.js
+++ game-eg/tests.js
@@ -79,6 +79,17 @@ test.tests = function() {
   test.assertEqual("The book is here.", processText("{nm:chr:the:true} is here.", {chr:w.book}));
 
   
+  test.title("Text processor 5");
+  test.assertEqual("Kyle is a bear.", processText("{Kyle.alias} is a bear."));
+  test.assertEqual("Kyle is a bear.", processText("{show:Kyle:alias} is a bear."));
+  test.assertEqual("Kyle is a bear.", processText("{Kyle:alias} is a bear."));
+  test.assertEqual("You have $10.", processText("You have ${show:me:money}."));
+  test.assertEqual("You have $10.", processText("You have ${player.money}."));
+  test.assertEqual("You have $10.", processText("You have ${me.money}."));
+  test.assertEqual("You have $10.", processText("You have ${player.money}."));
+
+
+
   test.title("Numbers");
   test.assertEqual("fourteen", toWords(14));
   test.assertEqual("minus four hundred and three", toWords(-403));
