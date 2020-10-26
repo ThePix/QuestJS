b232515b1a6893f4d7c9475c88e87df397b934a6
ThePix <the_pix@hotmail.com>
2020-10-26 20:04:17 +0000

    More map tweaks

---
+++ game-alt-map/settings.js
@@ -0,0 +1,9 @@
+"use strict"
+
+settings.title = "The City of Halmuth"
+settings.author = "The Pixie"
+settings.version = "0.1"
+settings.thanks = []
+settings.warnings = "No warnings have been set for this game."
+settings.playMode = "dev"
+settings.symbolsForCompass = true
