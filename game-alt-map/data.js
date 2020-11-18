d6ef69fcfdf246d5f6be6c5549c710979ca57127
ThePix <the_pix@hotmail.com>
2020-11-18 16:10:31 +0000

    Update to dateTime

--- game-alt-map/data.js
+++ game-alt-map/data.js
@@ -8,7 +8,7 @@ createItem("me", PLAYER(), {
 })
 
 createRoom("street_of_the_gods", {
-  desc:"The street heads north to the High Temple, and south to the market square, and is lined with stone-built buildings reach three or even four storeys into the sky, with towers, turrets ans spires reaching even higher. Grandest of all is the temple itself, standing on a platform that raises it above the rest of the city, accessed via a wide flight of steps.",
+  desc:"The street rises to north up to the High Temple, and drops south to the market square. It is lined with stone-built buildings reaching three or even four storeys into the sky, with towers, turrets and spires reaching even higher. Grandest of all is the temple itself, standing on a platform that raises it high above the rest of the city, accessed via a wide flight of steps, but you also see the Museum of Curios to the west and the House of Elil to the southeast, with the condensers of the Aether and Flux Company behind it.{timeOfDayComment}",
   west:new Exit('museum_of_curios'),
   south:new Exit('market_square'),
   north:new Exit('high_temple'),
@@ -21,20 +21,38 @@ createRoom("street_of_the_gods", {
 
 
 createRoom("market_square", {
-  desc:"The market square is the centre of the city, and is teeming with people. There are dozens of stalls, selling everything from mushrooms to mallets, from songbirds to shawls.",
+  desc:"The market square is the centre of the city, and is teeming with people. {timeOfDayComment} In the southeast corner you see the magnificent entrance of Estalia Manor.|In the centre of the square, a golden statue of Stratos and Geo embracing while looking north to the Great Temple stands on a stone plinth; their presence blesses all who pass through.",
   north:new Exit('street_of_the_gods'),
   west:new Exit('western_way_halmuth'),
   east:new Exit('wheat_road'),
   south:new Exit('merchants_street'),
+  southeast:new Exit('estalia_manor'),
   mapX:390,
   mapY:535,
+  timeStatus:[
+    {to:7, t:'The market is quiet at this time of night.'},
+    {to:8, t:'Market stall owners are setting out their wares for the day.'},
+    {to:13, t:'The market is full of people looking, buying and selling; there are dozens of stalls, selling everything from mushrooms to marquetry, from songbirds to shawls.'},
+    {to:16, t:'The market is quieter in the heat of the afternoon.'},
+    {to:17, t:'The market stalls are being packed away.'},
+    {to:25, t:'The market stalls are closed, but a few people are still wondering through the square.'},
+  ]
+})
+
+
+
+createItem("street_preacher", NPC(false), {
+  loc:"market_square",
+  scenery:true,
+  locationStatus:function() {
+    return "A preacher is stood on an old crate, telling people about woe and doom that will befall them soon; most of the crowd are happy to ignore him."
+  },
 })
 
 
 
 createRoom("wheat_road", {
-  desc:"The east side of Halmuth is the poorer side of the city. The tenement blocks on either are run down.",
-  northeast:new Exit('estalia_manor'),
+  desc:"The east side of Halmuth is the poorer side of the city. The tenement blocks on either are run down. Many have small shops or workshops on the ground floor, offering cheap goods of variable quality. Thandros' Arms and Armpour to the northwest and Madame Rel's Little Shop of Wonders to the south catch your eye; the brothel to the southeast certainly does not! The upper floors are fronted by balconies, that are often bedecked with flowers or banners. To the northeast, on the corner of Tuppenny Lane, you see the gates of the Aether and Flux Company; towering over the wall are the condensers, focal towers and retorts.",
   west:new Exit('market_square'),
   south:new Exit('quayside_east'),
   east:new Exit('granite_bridge'),
@@ -45,7 +63,7 @@ createRoom("wheat_road", {
 
 
 createRoom("merchants_street", {
-  desc:"Merchant's Street is boring, the author really needs to put stuff in it.",
+  desc:"Merchant's Street slopes steeply down from the market place in the north to the quayside to the south. It takes it name from the many high-class shops that line it on both sides, many selling imported goods. Renfikk's Alchemical Emporium to the northeast catches your eye, on the corner with a narrow backstreet. A covered walkway spans the backstreet connecting the shop to Ulgat's Fine Goods to the southeast.",
   north:new Exit('market_square'),
   south:new Exit('quayside_west'),
   mapX:385,
@@ -53,7 +71,7 @@ createRoom("merchants_street", {
 })
 
 createRoom("quayside_west", {
-  desc:"The west end of the Quayside is boring, the author really needs to put stuff in it.",
+  desc:"A three-story inn, The Leviathon, dominates the west end of the quayside, on the corner with Merchants' Street. Beyond it, you can see a couple of warehouses. Some fishing boats are moored to the quay.",
   north:new Exit('merchants_street'),
   east:new Exit('quayside'),
   mapX:380,
@@ -70,7 +88,7 @@ createRoom("quayside", {
 })
 
 createRoom("quayside_east", {
-  desc:"The west end of the Quayside is boring, the author really needs to put stuff in it.",
+  desc:"Warehouses line the east end of the quayside, storing goods inbound and outbound by ship. Most are in a state of disrepair. Some fishing boats are tied up on the quayside. A narrow road heads upwards and northwards to the Wheat Road; to the east you can see a boatyard.",
   west:new Exit('quayside'),
   north: new Exit('wheat_road'),
   mapX:720,
@@ -78,7 +96,7 @@ createRoom("quayside_east", {
 })
 
 createRoom("wharf", {
-  desc:"The wharf is boring, the author really needs to put stuff in it.",
+  desc:"The wharf is a sturdy structure, built of dark wood, projecting out from the quay some two hundred feet. Ships are moored on either side, and dockworks hurry to unload and then load them as fast as they can.",
   north:new Exit('quayside'),
   mapX:560,
   mapY:900,
@@ -86,7 +104,7 @@ createRoom("wharf", {
 
 
 createRoom("western_way_halmuth", {
-  desc:"The Western Way is boring, the author really needs to put stuff in it. Further west takes you out of the city.",
+  desc:"This is the start of the Western Way that connects Halmuth to Ogarath. To the east of you is the market square, the actual start of the road, and to the west the city gates. To the northeast, the tall building is the back of the Museum of Curios, whilst the pillared building to the south is the House of Leetos.",
   east:new Exit('market_square'),
   west:new Exit('western_way_1'),
   mapX:250,
@@ -131,7 +149,7 @@ createRoom("leviathon", {
 
 createRoom("estalia_manor", {
   desc:"Estalia Manor is boring, the author really needs to put stuff in it.",
-  southwest:new Exit('wheat_road'),
+  northwest:new Exit('market_square'),
   mapX:755,
   mapY:477,
 })
