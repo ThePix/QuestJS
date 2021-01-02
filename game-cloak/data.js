01a729e78d183f7ff6aea3b1845e4c03faccc7f6
ThePix <the_pix@hotmail.com>
2021-01-02 16:40:16 +0000

    Corrected buf in msgTable and msgList
    Thanks to xelx0r
    https://github.com/ThePix/QuestJS/issues/57

--- game-cloak/data.js
+++ game-cloak/data.js
@@ -10,32 +10,6 @@
 
 
 
-const cloakHere = function() {
-  if (w.cloak.isAtLoc('me')) return true
-  if (w.cloak.isHere()) return true
-  if (w.cloak.isAtLoc('hook') && game.player.isAtLoc('cloakroom')) return true
-  return false
-}
-
-
-lang.no_smell = "It smells slightly musty."
-
-lang.no_listen = "It is quiet as the grave..."
-
-
-tp.addDirective("cloakHere", function(arr, params) {
-  return cloakHere() ? arr[0] : arr[1]
-});
-
-findCmd('MetaCredits').script = function() {
-  metamsg('This game was created by The Pixie, following the Cloak of Darkness specification by Roger Firth.')
-}
-
-findCmd('MetaHelp').script = function() {
-  metamsg('Just type stuff at the prompt!')
-}
-
-
 
 
 
@@ -253,65 +227,4 @@ createItem('doors', {
     return w[loc].room && situation === world.PARSER; 
   },
 })
-
-
-
-
-commands.push(new Cmd('HangUp', {
-  regex:/^(hang up|hang) (.+)$/,
-  objects:[
-    {ignore:true},
-    {scope:parser.isHeld},
-  ],
-  script:function(objects) {
-    if (!objects[0][0].isAtLoc(game.player)) {
-      return failedmsg ("You're not carrying {sb:obj}.", {obj:objects[0][0]})
-    }
-    else if (objects[0][0].worn) {
-      return failedmsg ("Not while you're wearing {sb:obj}!", {obj:objects[0][0]})
-    }
-    else if (!game.player.isAtLoc('cloakroom')) {
-      return failedmsg ("Hang {sb:obj} where, exactly?", {obj:objects[0][0]})
-    }
-    else {
-      objects[0][0].moveToFrom('hook')
-      msg ("You hang {nm:obj:the} on the hook.", {obj:objects[0][0]})
-      return world.SUCCESS
-    }
-  }  
-}))
-
-
-
-commands.push(new Cmd('HangUp', {
-  regex:/^(hang up|hang) (.+) on (|the )hook$/,
-  objects:[
-    {ignore:true},
-    {scope:parser.isHeld},
-    {ignore:true},
-  ],
-  script:function(objects) {
-    if (!objects[0][0].isAtLoc(game.player)) {
-      return failedmsg ("You're not carrying {sb:obj}.", {obj:objects[0][0]})
-    }
-    else if (objects[0][0].worn) {
-      return failedmsg ("Not while you're wearing {sb:obj}!", {obj:objects[0][0]})
-    }
-    else if (!game.player.isAtLoc('cloakroom')) {
-      return failedmsg ("Hang {sb:obj} where, exactly?", {obj:objects[0][0]})
-    }
-    else {
-      objects[0][0].moveToFrom('hook')
-      msg ("You hang {nm:obj:the} on the hook.", {obj:objects[0][0]})
-      return world.SUCCESS
-    }
-  }  
-}))
-
-
-
-
-
-
-
 
--- game-cloak/settings.js
+++ game-cloak/settings.js
@@ -1,10 +1,11 @@
-"use strict";
+"use strict"
 
 // About your game
-settings.title = "Cloak of Darkness";
+settings.title = "Cloak of Darkness"
 settings.author = "The Pixie"
-settings.version = "0.3";
-settings.thanks = [];
+settings.version = "0.3"
+settings.thanks = []
+settings.ifid = '1F95711E-44DE-4C57-AA30-0BAB292E5874'
 
 settings.panes = 'none'
 
